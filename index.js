64547a4c0911e99827e7f1245c0d5e4939383fac
nati8333 <the.defalt8@gmail.com>
2024-08-12 17:23:49 +0300

    switch to relational method

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Types;
 
 const app = express();
 
@@ -15,26 +16,24 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("connected to database"))
   .catch(console.error);
 
-const options = {};
-
-const exerciseSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true }
-}, options);
-
-const Exercise = mongoose.model("Exercise", exerciseSchema);
-
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  exercises: [exerciseSchema]
-}, options);
+  username: { type: String, required: true }
+});
 
 const User = mongoose.model("User", userSchema);
 
+const exerciseSchema = new mongoose.Schema({
+  userId: { type: ObjectId, required: true, ref: User },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true }
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
 app.post("/api/reset", (req, res) => {
-  Exercise.deleteMany();
-  User.deleteMany();
+  Exercise.deleteMany({}).catch(console.error);
+  User.deleteMany({}).catch(console.error);
   return res.sendStatus(200);
 });
 
@@ -43,43 +42,41 @@ app.post("/api/users", (req, res) => {
   const newUser = new User({ username });
   newUser.save()
     .then(userDoc => res.json({
-      username: userDoc.username,
-      _id: userDoc._id
+      _id: userDoc._id,
+      username: userDoc.username
     }))
     .catch(error => res.json({ error: error.message }));
 });
 
 app.get("/api/users", (req, res) => {
   User.find()
-    .then(usersDoc => res.json(usersDoc.map(userDoc => ({
-      username: userDoc.username,
-      _id: userDoc._id,
-    }))))
+    .select({ _id: 1, username: 1 })
+    .exec()
+    .then(usersDoc => res.json(usersDoc))
     .catch(error => res.json({ error: error.message }));
 });
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   const _id = req.params._id;
-  const description = req.body.description;
-  const duration = req.body.duration;
-  const date = req.body.date ? new Date(req.body.date) : new Date();
-  const exercise = new Exercise({
-    description,
-    duration,
-    date
-  });
-  // exercise.save();
   User.findById(_id)
-    .then(userDoc => {
-      userDoc.exercises.push(exercise);
-      userDoc.save()
-        .then(() => res.json({
-          username: userDoc.username,
-          description: exercise.description,
-          duration: exercise.duration,
-          date: exercise.date.toDateString(),
-          _id: userDoc._id
-        }))
+    .exec()
+    .then((userDoc) => {
+      const exercise = new Exercise({
+        userId: _id,
+        description: req.body.description,
+        duration: req.body.duration,
+        date: req.body.date ? new Date(req.body.date) : new Date()
+      });
+      exercise.save()
+        .then((exerciseDoc) => {
+          return res.json({
+            _id: exerciseDoc.userId,
+            username: userDoc.username,
+            description: exerciseDoc.description,
+            duration: exerciseDoc.duration,
+            date: exerciseDoc.date,
+          });
+        })
         .catch(error => res.json({ error: error.message }));
     })
     .catch(error => res.json({ error: error.message }));
@@ -89,53 +86,40 @@ app.get("/api/users/:_id/logs", (req, res) => {
   const _id = req.params._id;
   const from = req.query.from
     ? new Date(req.query.from)
-    : null;
+    : new Date(0);
   const to = req.query.to
     ? new Date(req.query.to)
     : null;
   const limit = req.query.limit
     ? Number(req.query.limit)
-    : null;
-  let counter = limit;
+    : 0;
   const extras = {};
   if (from) extras.from = from.toDateString();
   if (to) extras.to = to.toDateString();
+  const dateFilter = (from && to)
+    ? { $gte: from, $lte: to }
+    : (from ? { $gte: from } : {});
   User.findById(_id)
-    .select({
-      username: 1,
-      _id: 1,
-      exercises: 1
-    })
     .exec()
-    .then(log => {
-      const sortedExercises = log.exercises.sort((a, b) => b.date.getTime() - a.date.getTime());
-      const exercises = (from || to || limit)
-        ? sortedExercises.filter(exercise => {
-          if (from && to && limit)
-            return counter-- > 0
-              && exercise.date.getTime() >= from.getTime()
-              && exercise.date.getTime() <= to.getTime();
-          else if (from && to)
-            return exercise.date.getTime() >= from.getTime()
-              && exercise.date.getTime() <= to.getTime();
-          else if (from)
-            return exercise.date.getTime() >= from.getTime();
-          else if (limit)
-            return counter-- > 0;
-          return true;
+    .then((userDoc) => {
+      Exercise.find({ userId: userDoc._id })
+        .sort({ date: -1 })
+        .find({ date: dateFilter })
+        .limit(limit)
+        .exec()
+        .then(exerciseDocs => {
+          return res.json({
+            _id: exerciseDocs.userId,
+            username: userDoc.username,
+            count: exerciseDocs.length,
+            log: exerciseDocs.map(exerciseDoc => ({
+              description: exerciseDoc.description,
+              duration: exerciseDoc.duration,
+              date: exerciseDoc.date.toDateString(),
+            }))
+          });
         })
-        : sortedExercises;
-      return res.json({
-        username: log.username,
-        ...extras,
-        count: exercises.length,
-        _id: log._id,
-        log: exercises.map(exercise => ({
-          description: exercise.description,
-          duration: exercise.duration,
-          date: exercise.date.toDateString()
-        }))
-      });
+        .catch(error => res.json({ error: error.message }));
     })
     .catch(error => res.json({ error: error.message }));
 });
